3495bd4a99be57a4a98e3d6a407e35154b2a0685
Mike Slinn <mslinn@mslinn.com>
2014-07-12 10:17:32 -0700

    Added sound effects

--- src/main/scala/OnCreate.scala
+++ src/main/scala/OnCreate.scala
@@ -1,4 +1,4 @@
-case class FiletypeAction(filetype: String, commandTokens: List[String])
+case class FiletypeAction(filetype: String, commandTokens: List[String], soundStart: String="", soundEnd: String="")
 
 object OnCreate extends App {
   import collection.JavaConverters._
@@ -6,6 +6,7 @@ object OnCreate extends App {
   import com.typesafe.config.{Config, ConfigObject}
   import com.beachape.filemanagement.RegistryTypes._
   import com.beachape.filemanagement.Messages._
+  import java.io.File
   import java.nio.file.Path
   import java.nio.file.StandardWatchEventKinds._
 
@@ -15,16 +16,36 @@ object OnCreate extends App {
   def playSound(fileName: String): Unit = {
     import java.net.URL
     import javax.sound.sampled._
+    import scala.concurrent.Await
+    import scala.concurrent.duration._
 
     try {
-      val url = new URL(fileName)
-      val audioIn = AudioSystem.getAudioInputStream(url)
+      import scala.concurrent.Promise
+      val audioIn: AudioInputStream = {
+        if (fileName.startsWith("http:"))
+          AudioSystem.getAudioInputStream(new URL(fileName) )
+        else {
+          val file: File = new File(getClass.getClassLoader.getResource(fileName).getFile)
+          println(s"Reading from ${file.getCanonicalPath}")
+          AudioSystem.getAudioInputStream(file)
+        }
+      }
       val clip = AudioSystem.getClip
       clip.open(audioIn)
       clip.start()
+      val promise = Promise[String]()
+      clip.addLineListener(new LineListener { // wait until sound has finished playing
+        def update(event: LineEvent): Unit = {
+          if (event.getType == LineEvent.Type.STOP) {
+            event.getLine.close()
+            promise.success("done")
+          }
+        }
+      })
+      Await.result(promise.future, Duration.Inf)
     } catch {
       case e: Exception =>
-        println(e.getMessage)
+        println(e)
     }
   }
 
@@ -35,15 +56,17 @@ object OnCreate extends App {
       val commandTokens: mutable.Buffer[String] = config.getStringList("commandTokens").asScala
       val command = commandTokens.map(_.replaceAll("\\$f", path.toString))
       println(s"$path was created so the following is about to be executed: $command")
-      playSound("http://home.comcast.net/~jdeshon2/wave_files/jad0001a.wav")
-      try {
-        val result = Process(command).!!.trim
-        println(result)
+      playSound("ascending.wav")
+      val startMillis = System.currentTimeMillis
+      val result = try {
+        Process(command).!
       } catch {
         case e: Exception =>
           println(e.getMessage)
       }
-      playSound("http://home.comcast.net/~jdeshon2/wave_files/jad0001a.wav")
+      val endMillis = System.currentTimeMillis
+      if (endMillis-startMillis<250) Thread.sleep(250)
+      playSound(if (result==0) "descending.wav" else "problem.wav")
     } else {
       println(s"$path does not end with $filetype so action was not triggered.")
     }
@@ -58,14 +81,20 @@ object OnCreate extends App {
     }
   }
 
-  val watchedDirectory = if (args.nonEmpty) args(0) else new java.io.File("").getAbsolutePath
-  val watchedPath = java.nio.file.Paths get watchedDirectory
+  val watchedFile = new File(if (args.nonEmpty) args(0) else "").getCanonicalFile
+  val watchedDirectory = watchedFile.getCanonicalPath
+  if (!watchedFile.exists) {
+    println(s"Fatal: $watchedDirectory does not exist.")
+    sys.exit(-1)
+  }
   println(s"Watching $watchedDirectory")
+
+  val x = watchedFile.toPath
 
   fileMonitorActor ! RegisterCallback(
     ENTRY_CREATE,
     modifier = None,
-    recursive = false,
-    path = watchedPath,
+    recursive = true,
+    path = watchedFile.toPath,
     createCallback)
 }
