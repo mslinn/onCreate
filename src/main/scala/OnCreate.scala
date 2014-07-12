98209bb170cd8510f186e7c6831abb30c9c0cb39
Mike Slinn <mslinn@mslinn.com>
2014-07-12 07:36:25 -0700

    Playing with sound

--- src/main/scala/OnCreate.scala
+++ src/main/scala/OnCreate.scala
@@ -12,6 +12,17 @@ object OnCreate extends App {
   implicit val system = akka.actor.ActorSystem("actorSystem")
   val fileMonitorActor = system.actorOf(com.beachape.filemanagement.MonitorActor())
 
+  def playSound(fileName: String): Unit = {
+    import java.net.URL
+    import javax.sound.sampled._
+
+    val url = new URL(fileName)
+    val audioIn = AudioSystem.getAudioInputStream(url)
+    val clip = AudioSystem.getClip
+    clip.open(audioIn)
+    clip.start()
+  }
+
   def handleActions(path: Path, config: Config) = {
     import sys.process._
     val filetype: String = config.getString("filetype")
@@ -19,8 +30,10 @@ object OnCreate extends App {
       val commandTokens: mutable.Buffer[String] = config.getStringList("commandTokens").asScala
       val command = commandTokens.map(_.replaceAll("\\$f", path.toString))
       println(s"$path was created so the following is about to be executed: $command")
+      playSound("http://mywebpages.comcast.net/jdeshon2/wave_files/jad0001a.wav")
       val result = Process(command).!!.trim
       println(result)
+      playSound("http://mywebpages.comcast.net/jdeshon2/wave_files/jad0001a.wav")
     } else {
       println(s"$path does not end with $filetype so action was not triggered.")
     }
